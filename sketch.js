358670280d14bbcb56b0246c12efe616159f0e44
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-05-03 12:12:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,22 @@
-var zenia,tourus,cyclap,zeniaDeform,tourusDeform,cyclapDeform,wall1,wall2,wall3,zeniaWeight,zeniaSpeed;
+var zenia,tourus,cyclap,zeniaDeform,tourusDeform,cyclapDeform,wall1,wall2,wall3,zeniaWeight,zeniaSpeed,tourusSpeed,tourusWeight,cyclapSpeed,cyclapWeight;
 
 function setup() {
   createCanvas(1600,400);
   zenia = createSprite(100, 100, 50, 50);
   zenia.shapeColor = "white";
   zenia.velocityX = 10;
-  zenia.debug = true;
-  zeniaSpeed = 60;
-  zeniaWeight = 2260;
+  zeniaSpeed = random(40,60);
+  zeniaWeight = random(2160,2260);
   tourus = createSprite(100,200,50,50);
   tourus.shapeColor = "white";
+  tourusSpeed = random(30,50);
+  tourusWeight = random(1422,1522);
+  tourus.velocityX = 10;
   cyclap = createSprite(100,300,50,50);
   cyclap.shapeColor = "white";
+  cyclapSpeed = random(25,45);
+  cyclapWeight = random(2900,3000);
+  cyclap.velocityX = 10;
   wall1 = createSprite(1200,100,30,75);
   wall1.shapeColor = "black";
   wall2 = createSprite(1200,200,30,75);
@@ -46,7 +51,58 @@ function draw() {
 
   }
 
-  drawSprites();
+}
+
+  if(tourus.x-wall2.x<=(tourus.width/2+wall2.width/2)&&wall2.x-tourus.x<=(tourus.width/2+wall2.width/2)){
+
+    tourusDeform = 0.5*tourusWeight*tourusSpeed*tourusSpeed/22500;
+    tourus.velocityX = 0;
+
+  if(tourusDeform >= 80){
+
+    tourus.shapeColor = "green";
+
+  }
+
+  if(tourusDeform >= 120){
+
+    tourus.shapeColor = "yellow";
+
+  }
+
+  if(tourusDeform >= 180){
+
+    tourus.shapeColor = "red";
+
   }
 }
 
+
+  if(cyclap.x-wall3.x<=(cyclap.width/2+wall3.width/2)&&wall3.x-cyclap.x<=(cyclap.width/2+wall3.width/2)){
+
+    cyclapDeform = 0.5*cyclapWeight*cyclapSpeed*cyclapSpeed/22500;
+    cyclap.velocityX = 0;
+
+  if(cyclapDeform >= 80){
+
+    cyclap.shapeColor = "green";
+
+  }
+
+  if(cyclapDeform >= 120){
+
+    cyclap.shapeColor = "yellow";
+
+  }
+
+  if(cyclapDeform >= 180){
+
+    cyclap.shapeColor = "red";
+
+  }
+
+}  
+
+  drawSprites();
+
+}
